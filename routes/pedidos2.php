e7fafc2804423ce5bd8747039d71f77371f0db9f
IsaiElizondo <isai.elizondom@gmail.com>
2025-04-10 12:04:35 -0600

    Cambio de formato del directorio Routes del 8 al 9

--- routes/pedidos2.php
+++ routes/pedidos2.php
@@ -4,92 +4,84 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 
-Route::group(['middleware' => 'auth'], function () {
+use App\Http\Controllers\Pedidos2Controller;
 
-	Route::get('pedidos2/', 'Pedidos2Controller@index');
-	Route::get('pedidos2/index', 'Pedidos2Controller@index');
-	Route::get('pedidos2/lista', 'Pedidos2Controller@lista');
+Route::middleware('auth')->group(function () {
+    Route::get('pedidos2/', [Pedidos2Controller::class, 'index']);
+	Route::get('pedidos2/index', [Pedidos2Controller::class, 'index']);
+	Route::get('pedidos2/lista', [Pedidos2Controller::class, 'lista']);
 
-	Route::get('pedidos2/nuevo', 'Pedidos2Controller@nuevo');
-	Route::post('pedidos2/guardar/{id}', 'Pedidos2Controller@guardar');
-	Route::post('pedidos2/crear', 'Pedidos2Controller@crear');
+	Route::get('pedidos2/nuevo', [Pedidos2Controller::class, 'nuevo']);
+	Route::post('pedidos2/guardar/{id}', [Pedidos2Controller::class, 'guardar']);
+	Route::post('pedidos2/crear', [Pedidos2Controller::class, 'crear']);
 
-	Route::get('pedidos2/pedido/{id}', 'Pedidos2Controller@pedido');
-	Route::get('pedidos2/masinfo/{id}', 'Pedidos2Controller@masinfo');
-	Route::get('pedidos2/historial/{id}', 'Pedidos2Controller@historial');
-	Route::get('pedidos2/fragmento/{id}/{cual}', 'Pedidos2Controller@fragmento');
+	Route::get('pedidos2/pedido/{id}', [Pedidos2Controller::class, 'pedido']);
+	Route::get('pedidos2/masinfo/{id}', [Pedidos2Controller::class, 'masinfo']);
+	Route::get('pedidos2/historial/{id}', [Pedidos2Controller::class, 'historial']);
+	Route::get('pedidos2/fragmento/{id}/{cual}', [Pedidos2Controller::class, 'fragmento']);
 
-	Route::get('pedidos2/entregar_edit/{id}', 'Pedidos2Controller@entregar_edit');
-	Route::get('pedidos2/set_accion_entregar/{id}', 'Pedidos2Controller@set_accion_entregar');
-	Route::get('pedidos2/set_parcial_status/{id}', 'Pedidos2Controller@set_parcial_status');
-	Route::get('pedidos2/unset_entregado/{id}', 'Pedidos2Controller@unset_entregado');
+	Route::get('pedidos2/entregar_edit/{id}', [Pedidos2Controller::class, 'entregar_edit']);
+	Route::get('pedidos2/set_accion_entregar/{id}', [Pedidos2Controller::class, 'set_accion_entregar']);
+	Route::get('pedidos2/set_parcial_status/{id}', [Pedidos2Controller::class, 'set_parcial_status']);
+	Route::get('pedidos2/unset_entregado/{id}', [Pedidos2Controller::class, 'unset_entregado']);
 
-	Route::get('pedidos2/parcial_nuevo/{id}', 'Pedidos2Controller@parcial_nuevo');	
-	Route::post('pedidos2/parcial_crear/{id}', 'Pedidos2Controller@parcial_crear');
-	Route::get('pedidos2/parcial_edit/{id}', 'Pedidos2Controller@parcial_edit');
-	Route::post('pedidos2/parcial_update/{id}', 'Pedidos2Controller@parcial_update');
-	Route::get('pedidos2/parcial_lista/{id}', 'Pedidos2Controller@parcial_lista');
-	                            
+	Route::get('pedidos2/parcial_nuevo/{id]', [Pedidos2Controller::class, 'parcial_nuevo']);
+	Route::post('pedidos2/parcial_crear/{id}', [Pedidos2Controller::class, 'parcial_crear']);
+	Route::get('pedidos2/parcial_edit/{id}', [Pedidos2Controller::class, 'parcial_edit']);
+	Route::post('pedidos2/parcial_update/{id}', [Pedidos2Controller::class, 'parcial_update']);
+	Route::get('pedidos2/parcial_lista/{id}', [Pedidos2Controller::class, 'parcial_lista']);
 
-	Route::get('pedidos2/subproceso_nuevo/{id}', 'Pedidos2Controller@subproceso_nuevo');
-	Route::post('pedidos2/smaterial_crear/{id}', 'Pedidos2Controller@smaterial_crear');
-	Route::get('pedidos2/smaterial_edit/{id}', 'Pedidos2Controller@smaterial_edit');
-	Route::post('pedidos2/smaterial_update/{id}', 'Pedidos2Controller@smaterial_update');
-	Route::get('pedidos2/smaterial_lista/{id}', 'Pedidos2Controller@smaterial_lista');
-	Route::get('pedidos2/set_smaterial_status/{id}', 'Pedidos2Controller@set_smaterial_status');
+	Route ::get('pedidos2/subproceso_nuevo/{id}', [Pedidos2Controller::class, 'subproceso_nuevo']);
+	Route::post('pedidos2/smaterial_crear/{id}', [Pedidos2Controller::class, 'smaterial_crear']);
+	Route::get('pedidos2/smaterial_edit/{id}', [Pedidos2Controller::class, 'smaterial_edit']);
+	Route::post('pedidos2/smaterial_update/{id}', [Pedidos2Controller::class, 'smaterial_update']);
+	Route::get('pedidos2/smaterial_lista/{id}', [Pedidos2Controller::class, 'smaterial_lista']);
+	Route::get('pedidos2/set_smaterial_status/{id}', [Pedidos2Controller::class, 'set_smaterial_status']);
 
+	Route::post('pedidos2/ordenf_crear/{id}', [Pedidos2Controller::class, 'ordenf_crear']);
+	Route::get('pedidos2/ordenf_edit/{id}', [Pedidos2Controller::class, 'ordenf_edit']);
+	Route::post('pedidos2/ordenf_update/{id}', [Pedidos2Controller::class, 'ordenf_update']);
+	Route::get('pedidos2/ordenf_lista/{id}', [Pedidos2Controller::class, 'ordenf_lista']);
 
-	Route::post('pedidos2/ordenf_crear/{id}', 'Pedidos2Controller@ordenf_crear');
-	Route::get('pedidos2/ordenf_edit/{id}', 'Pedidos2Controller@ordenf_edit');
-	Route::post('pedidos2/ordenf_update/{id}', 'Pedidos2Controller@ordenf_update');
-	Route::get('pedidos2/ordenf_lista/{id}', 'Pedidos2Controller@ordenf_lista');
+	Route::post('pedidos2/requisicion_crear/{id}', [Pedidos2Controller::class, 'requisicion_crear']);
+	Route::get('pedidos2/requisicion_edit/{id}', [Pedidos2Controller::class, 'requisicion_edit']);
+	Route::post('pedidos2/requisicion_update/{id}', [Pedidos2Controller::class, 'requisicion_update']);
+	Route::get('pedidos2/requisicion_lista/{id}', [Pedidos2Controller::class, 'requisicion_lista']);
 
-	Route::post('pedidos2/requisicion_crear/{id}', 'Pedidos2Controller@requisicion_crear');
-	Route::get('pedidos2/requisicion_edit/{id}', 'Pedidos2Controller@requisicion_edit');
-	Route::post('pedidos2/requisicion_update/{id}', 'Pedidos2Controller@requisicion_update');
-	Route::get('pedidos2/requisicion_lista/{id}', 'Pedidos2Controller@requisicion_lista');
+	Route::get('pedidos2/devolucion_lista/{id}', [Pedidos2Controller::class, 'devolucion_lista']);
+	Route::get('pedidos2/devolucion_edit/{id}', [Pedidos2Controller::class, 'devolucion_edit']);
+	Route::post('pedidos2/devolucion_update/{id}', [Pedidos2Controller::class, 'devolucion_update']);
+	Route::get('pedidos2/devolucion_nuevo/{order_id}', [Pedidos2Controller::class, 'devolucion_nuevo']);
+	Route::post('pedidos2/devolucion_crear/{id}', [Pedidos2Controller::class, 'devolucion_crear']);
 
+	Route::get('pedidos2/refacturacion_lista/{id}', [Pedidos2Controller::class, 'refacturacion_lista']);
+	Route::get('pedidos2/refacturacion_edit/{id}', [Pedidos2Controller::class, 'refacturacion_edit']);
+	Route::post('pedidos2/refacturacion_update/{id}', [Pedidos2Controller::class, 'refacturacion_update']);
+	Route::get('pedidos2/refacturacion_nuevo/{order_id}', [Pedidos2Controller::class, 'refacturacion_nuevo']);
+	Route::post('pedidos2/refacturacion_crear/{id}', [Pedidos2Controller::class, 'refacturacion_crear']);
 
-	Route::get('pedidos2/devolucion_lista/{id}', 'Pedidos2Controller@devolucion_lista');
-	Route::get('pedidos2/devolucion_edit/{id}', 'Pedidos2Controller@devolucion_edit');
-	Route::post('pedidos2/devolucion_update/{id}', 'Pedidos2Controller@devolucion_update');
-	Route::get('pedidos2/devolucion_nuevo/{order_id}', 'Pedidos2Controller@devolucion_nuevo');
-	Route::post('pedidos2/devolucion_crear/{id}', 'Pedidos2Controller@devolucion_crear');
-
-
-	Route::get('pedidos2/refacturacion_lista/{id}', 'Pedidos2Controller@refacturacion_lista');
-	Route::get('pedidos2/refacturacion_edit/{id}', 'Pedidos2Controller@refacturacion_edit');
-	Route::post('pedidos2/refacturacion_update/{id}', 'Pedidos2Controller@refacturacion_update');
-	Route::get('pedidos2/refacturacion_nuevo/{order_id}', 'Pedidos2Controller@refacturacion_nuevo');
-	Route::post('pedidos2/refacturacion_crear/{id}', 'Pedidos2Controller@refacturacion_crear');
-
-	Route::get('pedidos2/stockreq_edit/{id}', 'Pedidos2Controller@stockreq_edit');
-	Route::post('pedidos2/stockreq_update/{id}', 'Pedidos2Controller@stockreq_update');
-	Route::post('pedidos2/set_accion_surters/{id}', 'Pedidos2Controller@set_accion_surters');
+	Route::get('pedidos2/stockreq_edit/{id}', [Pedidos2Controller::class, 'stockreq_edit']);
+	Route::post('pedidos2/stockreq_update/{id}', [Pedidos2Controller::class, 'stockreq_update']);
+	Route::post('pedidos2/set_accion_surters/{id}', [Pedidos2Controller::class, 'set_accion_surters']);
 	
-	
-	Route::get('pedidos2/shipment_edit/{id}', 'Pedidos2Controller@shipment_edit');
-	Route::post('pedidos2/shipment_update/{id}', 'Pedidos2Controller@shipment_update');
+	Route::get('pedidos2/shipment_edit/{id}', [Pedidos2Controller::class, 'shipment_edit']);
+	Route::post('pedidos2/shipment_update/{id}', [Pedidos2Controller::class, 'shipment_update']);
 
+	Route::get('pedidos2/accion/{id}', [Pedidos2Controller::class, 'accion']);
+	Route::post('pedidos2/set_accion/{id}', [Pedidos2Controller::class, 'set_accion']);
 
-	Route::get('pedidos2/accion/{id}', 'Pedidos2Controller@accion');
-	Route::post('pedidos2/set_accion/{id}', 'Pedidos2Controller@set_accion');
-	
-	Route::get('pedidos2/attachlist', 'Pedidos2Controller@attachlist');
-	Route::post('pedidos2/attachpost', 'Pedidos2Controller@attachpost');
-	Route::get('pedidos2/attachdelete', 'Pedidos2Controller@attachdelete');
+	Route::get('pedidos2/attachlist', [Pedidos2Controller::class, 'attachlist']);
+	Route::post('pedidos2/attachpost', [Pedidos2Controller::class, 'attachpost']);
+	Route::get('pedidos2/attachdelete', [Pedidos2Controller::class, 'attachdelete']);
 
+	Route::get('pedidos2/cancelar/{id}', [Pedidos2Controller::class, 'cancelar']);
+	Route::get('pedidos2/descancelar/{id}', [Pedidos2Controller::class, 'descancelar']);
 
-	Route::get('pedidos2/cancelar/{id}', 'Pedidos2Controller@cancelar');
-	Route::get('pedidos2/descancelar/{id}', 'Pedidos2Controller@descancelar');
-
-
-
-	Route::get('pedidos2/multie', 'Pedidos2Controller@multie');
-	Route::get('pedidos2/multie_lista', 'Pedidos2Controller@multie_lista');
-	Route::get('pedidos2/set_status/{id}', 'Pedidos2Controller@set_status');
-	Route::post('pedidos2/set_multistatus', 'Pedidos2Controller@set_multistatus');
-	Route::post('pedidos2/set_accion_desauditoria/{id}', 'Pedidos2Controller@set_accion_desauditoria');
-	Route::post('pedidos2/add_nota/{id}', 'Pedidos2Controller@add_nota');
-
+	Route::get('pedidos2/multie', [Pedidos2Controller::class, 'multie']);
+	Route::get('pedidos2/multie_lista', [Pedidos2Controller::class, 'multie_lista']);
+	Route::get('pedidos2/set_status/{id}', [Pedidos2Controller::class, 'set_status']);
+	Route::post('pedidos2/set_multistatus', [Pedidos2Controller::class, 'set_multistatus']);
+	Route::post('pedidos2/set_accion_desauditoria/{id}', [Pedidos2Controller::class, 'set_accion_desauditoria']);
+	Route::post('pedidos2/add_nota/{id}', [Pedidos2Controller::class, 'add_nota']);
 });
+
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,24 @@
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\CheckOrderController;
+use App\Http\Controllers\ArchivedController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\PictureController;
+use App\Http\Controllers\FollowsController;
+use App\Http\Controllers\CancelationsController;
+use App\Http\Controllers\RebillingsController;
+use App\Http\Controllers\DebolutionsController;
+use App\Http\Controllers\ShipmentsController;
+use App\Http\Controllers\ReportesController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,64 +32,64 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'WelcomeController@index')->name('welcome');
-Route::get('homesearch','WelcomeController@search')->name('homesearch');
+Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
+Route::get('/homesearch', [WelcomeController::class, 'search'])->name('homesearch');
 
 //Auth::routes();
 
-Route::get('home', 'HomeController@index')->name('home');
-Route::get('home/prueba', 'HomeController@prueba');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/home/prueba', [HomeController::class, 'prueba']);
+
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
+Route::get('/home', [HomeController::class, 'index'])->name('home')->middleware('auth');
 
 Route::group(['middleware' => 'auth'], function () {
-	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
-	Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
-	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::put('/profile/password', [ProfileController::class, 'password'])->name('profile.password');
 
-    Route::resource('users', 'UserController');
-    Route::resource('orders', 'OrderController');
-    Route::resource('check', 'CheckOrderController');
-    Route::resource('archived', 'ArchivedController');
-    Route::resource('roles', 'RoleController');
-    Route::resource('departments', 'DepartmentController');
-    Route::resource('picture', 'PictureController');
-    Route::resource('follows', 'FollowsController');
-    Route::resource('cancelations', 'CancelationsController');
-    Route::resource('rebillings', 'RebillingsController');
-    Route::resource('debolutions', 'DebolutionsController');
-    Route::resource('shipments', 'ShipmentsController');
+    Route::resource('users', UserController::class);
+    Route::resource('orders', OrderController::class);
+    Route::resource('checkorders', CheckOrderController::class);
+    Route::resource('archived', ArchivedController::class);
+    Route::resource('roles', RoleController::class);
+    Route::resource('departments', DepartmentController::class);
+    Route::resource('pictures', PictureController::class);
+    Route::resource('follows', FollowsController::class);
+    Route::resource('cancelations', CancelationsController::class);
+    Route::resource('rebillings', RebillingsController::class);
+    Route::resource('debolutions', DebolutionsController::class);
+    Route::resource('shipments', ShipmentsController::class);
 
-    Route::get('search','HomeController@search')->name('search');
-
-    Route::get('picture','HomeController@picture')->name('picture'); // Foto de entregado
-    Route::get('cancelation','HomeController@cancelation')->name('cancelation');
+    Route::get('serach', [HomeController::class, 'search'])->name('search');
+    Route::get('/picture', [HomeController::class, 'picture'])->name('picture');    //FOTO ENTREGADA
+    Route::get('/cancelation', [HomeController::class, 'cancelation'])->name('cancelation');
 
     // Rutas cancelación, refacturación y devolución
-    Route::get('shipmentEvidence','ShipmentsController@shipmentEvidence')->name('shipments.evidence');
+    Route::get('/shipmentEvidence', [ShipmentsController::class, 'shipmentEvidence'])->name('shipment.evidence'); //EVIDENCIA DE ENVIO
 
-    Route::get('cancelEvidence','CancelationsController@cancelEvidence')->name('cancelations.evidence');
-    Route::get('cancelRepayment','CancelationsController@cancelRepayment')->name('cancelations.repayment');
+    Route::get('/cancelEvidance', [CancelationsController::class, 'cancelEvidence'])->name('cancel.evidence'); //EVIDENCIA DE CANCELACION
+    Route::get('/cancelRepayment', [CancelationsController::class, 'cancelRepayment'])->name('cancel.repayment'); //REEMBOLSO DE CANCELACION
 
-    Route::get('rebillingEvidence','RebillingsController@rebilEvidence')->name('rebillings.evidence');
-    Route::get('rebillingRepayment','RebillingsController@rebilRepayment')->name('rebillings.repayment');
+    Route::get('/rebillingEvidence', [RebillingsController::class, 'rebillingEvidence'])->name('rebilling.evidence'); //EVIDENCIA DE REEMBOLSO
+    Route::get('/rebillingRepayment', [RebillingsController::class, 'rebillingRepayment'])->name('rebilling.repayment'); //REEMBOLSO DE REEMBOLSO
 
-    Route::get('debolutionsEvidence','DebolutionsController@debolutionsEvidence')->name('debolutions.evidence');
-    Route::get('debolutionsRepayment','DebolutionsController@debolutionsRepayment')->name('debolutions.repayment');
-    
-    
-    Route::get('reportes','ReportesController@index')->name('reportes');
-    Route::post('reportes/reporte','ReportesController@reporte')->name('reporte');
-    
-    
-    Route::get('order/attachlist', 'OrderController@attachlist')->name("attachlist");
-    Route::post('order/attachpost', 'OrderController@attachpost');
-    Route::get('order/attachdev', 'OrderController@attachdev');
-    Route::get('order/attachdelete', 'OrderController@attachdelete');
-    Route::get("order/debolutioncreatefor", "OrderController@debolutioncreatefor");
-    Route::get("order/partialcreatefor", "OrderController@partialcreatefor");
-    
+    Route::get('/debolutionEvidence', [DebolutionsController::class, 'debolutionEvidence'])->name('debolution.evidence'); //EVIDENCIA DE DEVOLUCION
+    Route::get('/debolutionRepayment', [DebolutionsController::class, 'debolutionRepayment'])->name('debolution.repayment'); //REEMBOLSO DE DEVOLUCION
+
+    Route::get('/reportes', [ReportesController::class, 'index'])->name('reportes.index');
+    Route::get('/reportes/resporte', [ReportesController::class, 'reporte'])->name('reporte');
+
+    Route::get('/order/attachlist', [OrderController::class, 'attachlist'])->name('order.attachlist');
+    Route::post('/order/attachpost', [OrderController::class, 'attachpost']);
+
+    Route::get('/order/attachdev', [OrderController::class, 'attachdev']);
+    Route::get('/order/attachdelete', [OrderController::class, 'attachdelete']);
+
+    Route::get('/order/debolutioncreatefor', [OrderController::class, 'debolutioncreatefor']);
+    Route::get('/order/partialcreatefor', [OrderController::class, 'partialcreatefor']);
+
 });
 
 require_once "pedidos2.php";
